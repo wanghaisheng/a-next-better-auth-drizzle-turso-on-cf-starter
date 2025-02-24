13bebfbcc114ff4292be92d4c42dbe3f81398417
Ignat S. <mail@igonato.com>
2025-02-24 11:18:49 +0300

    Prettier setup

---
+++ prettier.config.mjs
@@ -0,0 +1,11 @@
+export default {
+    tabWidth: 4,
+    overrides: [
+        {
+            files: ["*.yml", "*.yaml"],
+            options: {
+                tabWidth: 2,
+            },
+        },
+    ],
+};
